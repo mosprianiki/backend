08bf1db859e834f3df81cbc6f1c5cac0b3c006ef
Data-Name-ID <data.name.id@yandex.ru>
2025-05-20 10:59:14 +0300

    feat: models

--- app/api/project/models.py
+++ app/api/project/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Numeric, String
+from sqlalchemy import ForeignKey, Numeric, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.api.project.enums import Approach
@@ -15,35 +15,50 @@ class Projects(BaseModel):
         unique=True,
     )
 
-    input_data = relationship(back_populates="projects")
-    output_data = relationship(back_populates="projects")
+    intersections = relationship(back_populates="projects")
 
 
-class InputData(BaseModel):
+class Intersection(BaseModel):
+    __tablename__ = "intersections"
+    __table_args__ = (
+        UniqueConstraint(
+            "id",
+            "project_id",
+            name="intersection_project_uc",
+        ),
+    )
+
+    id: Mapped[int]
+    project_id: Mapped[int] = mapped_column(ForeignKey("projects.project_id"))
+
+
+class FlowsInputData(BaseModel):
     __tablename__ = "input_data"
 
-    input_id: Mapped[int] = mapped_column(primary_key=True)
-    project_id: Mapped[int] = mapped_column(ForeignKey("projects.project_id"))
-    intersection_id: Mapped[int]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    intersection_id: Mapped[int] = mapped_column(ForeignKey("intersections.id"))
     approach: Mapped[Approach]
     intensity_veh_per_hr: Mapped[int]
     bus_share: Mapped[int] = mapped_column(Numeric(6, 3))
     len_between_intersections: Mapped[int]
     avg_speed: Mapped[int]
 
-    projects = relationship(ack_populates="input_data")
+
+class RelationsInputData(BaseModel):
+    __tablename__ = "relation_input_data"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_intersection_id: Mapped[int] = mapped_column(ForeignKey("intersections.id"))
+    second_intersection_id: Mapped[int] = mapped_column(ForeignKey("intersections.id"))
+    len_between_intersections: Mapped[int]
+    avg_speed: Mapped[int]
 
 
 class OutputData(BaseModel):
     __tablename__ = "output_data"
 
-    output_id: Mapped[int] = mapped_column(primary_key=True)
-    project_id: Mapped[int] = mapped_column(ForeignKey("projects.project_id"))
-    cycle_north_south: Mapped[int]
-    green_main_north_south: Mapped[int]
-    lost_time_north_south: Mapped[int]
-    cycle_east_west: Mapped[int]
-    green_main_east_west: Mapped[int]
-    lost_time_east_west: Mapped[int]
-
-    projects = relationship(back_populates="output_data")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    intersection_id: Mapped[int] = mapped_column(ForeignKey("intersections.id"))
+    cycle_sec: Mapped[int]
+    green_main_sec: Mapped[int]
+    green_secondary_sec: Mapped[int]
